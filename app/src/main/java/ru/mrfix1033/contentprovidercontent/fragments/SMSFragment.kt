f73de07294546312ff790881c8835a658d8051cb
Сергей Осипов <mrfix1033@mail.ru>
2025-01-29 20:44:57 +0300

    Toggle on sms sending

--- app/src/main/java/ru/mrfix1033/contentprovidercontent/fragments/SMSFragment.kt
+++ app/src/main/java/ru/mrfix1033/contentprovidercontent/fragments/SMSFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import ru.mrfix1033.contentprovidercontent.databinding.FragmentSMSBinding
 import ru.mrfix1033.contentprovidercontent.fragments.enumerations.Key
 import ru.mrfix1033.contentprovidercontent.models.ContactModel
+import ru.mrfix1033.contentprovidercontent.utils.TelephonyUtils
 
 class SMSFragment : Fragment() {
     private var _binding: FragmentSMSBinding? = null
@@ -29,7 +30,7 @@ class SMSFragment : Fragment() {
         binding.textViewName.text = contactModel.name
         binding.buttonSend.setOnClickListener {
             val message = binding.editTextMessage.text.toString()
-//            TelephonyUtils(requireContext()).sendSMS(contactModel.phoneNumber!!, message)
+            TelephonyUtils(requireContext()).sendSMS(contactModel.phoneNumber!!, message)
             requireActivity().supportFragmentManager.popBackStack()
         }
     }
